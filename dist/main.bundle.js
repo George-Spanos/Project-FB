39f4c997c705ec5b6eeff3c9cfd26b6150adca00
George Spanos <gespa11019@gmail.com>
2017-08-08 06:20:24 +0300

    yis

---
+++ dist/main.bundle.js
@@ -0,0 +1,390 @@
+webpackJsonp([1],{
+
+/***/ "../../../../../src async recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = "../../../../../src async recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".board {\r\n    display:block;\r\n    position:relative;\r\n    background-color:lightgrey;\r\n    width: 570px;\r\n    height:650px;\r\n    box-sizing: border-box;\r\n    margin: auto;\r\n    margin-top: 50px;\r\n     min-width:422px;\r\n     border-radius: 10px;\r\n}\r\n@media only screen and (max-width: 1024px) {\r\n    /* For mobile phones: */\r\n    .board {\r\n        width:60%;\r\n        height: 340px;\r\n        overflow: inherit;\r\n        background-color:lightgrey;\r\n        max-width: 422px;\r\n    }\r\n\r\n}\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"board\">\n  <app-card *ngFor=\"let card of mainArray\" [var]=\"card\">{{card.value}}{{card.text}} </app-card>\n</div>\n<button class=\"btn btn-primary col-xs-12\" style=\"width: 30%; margin:10px 35% \" (click)=\"this.gameFuntions.shuffleArray(mainArray)\">Shuffle Array</button>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__card_card_model__ = __webpack_require__("../../../../../src/app/card/card.model.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__game_functions_service__ = __webpack_require__("../../../../../src/app/game-functions.service.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var AppComponent = (function () {
+    function AppComponent(gameFuntions) {
+        this.gameFuntions = gameFuntions;
+    }
+    AppComponent.prototype.ngOnInit = function () {
+        this.mainArray = this.gameFuntions.InitializeArray();
+        console.log(this.mainArray);
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")],
+        providers: [__WEBPACK_IMPORTED_MODULE_2__game_functions_service__["a" /* GameFunctions */], __WEBPACK_IMPORTED_MODULE_1__card_card_model__["a" /* Card */]]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_2__game_functions_service__["a" /* GameFunctions */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__game_functions_service__["a" /* GameFunctions */]) === "function" && _a || Object])
+], AppComponent);
+
+var _a;
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__card_card_component__ = __webpack_require__("../../../../../src/app/card/card.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__game_functions_service__ = __webpack_require__("../../../../../src/app/game-functions.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__card_card_service__ = __webpack_require__("../../../../../src/app/card/card.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__card_card_model__ = __webpack_require__("../../../../../src/app/card/card.model.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_4__card_card_component__["a" /* CardComponent */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+        ],
+        providers: [__WEBPACK_IMPORTED_MODULE_5__game_functions_service__["a" /* GameFunctions */], __WEBPACK_IMPORTED_MODULE_6__card_card_service__["a" /* CardService */], __WEBPACK_IMPORTED_MODULE_7__card_card_model__["a" /* Card */]],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/card/card.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".card {\r\n  display:block;\r\n  /* position: absolute; */\r\n  background-image: linear-gradient(brown, grey);\r\n  height: 150px;\r\n  width:110px;\r\n  margin: 6px 16px;\r\n  float:left;\r\n  border-radius: 8px;\r\n}\r\n@media only screen and (max-width: 1024px) {\r\n    /* For mobile phones: */\r\n        .card{\r\n      position:auto;\r\n      height:69px;\r\n      width: 51px;\r\n      margin: 8px 27px;\r\n\r\n    }\r\n    .revealed{\r\n      font-size: 28px !important;\r\n    }\r\n}\r\n.revealed {\r\n  margin-top:30%;\r\n  font-size: 50px;\r\n  font-weight: Bold;\r\n  text-align:center;\r\n  color:white;\r\n}\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/card/card.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "  <div class=\"card\" (click)=\"Reveal()\">\n <p  class=\"revealed\" *ngIf=\"this.var.revealed\"><ng-content></ng-content></p>\n</div>\n\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/card/card.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__card_model__ = __webpack_require__("../../../../../src/app/card/card.model.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__card_service__ = __webpack_require__("../../../../../src/app/card/card.service.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return CardComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var CardComponent = (function () {
+    function CardComponent(cardService) {
+        this.cardService = cardService;
+    }
+    CardComponent.prototype.Reveal = function () {
+        var _this = this;
+        this.var.revealed = !this.var.revealed;
+        setTimeout(function () { _this.var.revealed = !_this.var.revealed; }, 4000);
+    };
+    CardComponent.prototype.ngOnInit = function () { };
+    return CardComponent;
+}());
+__decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Input */])(),
+    __metadata("design:type", typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__card_model__["a" /* Card */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__card_model__["a" /* Card */]) === "function" && _a || Object)
+], CardComponent.prototype, "var", void 0);
+CardComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* Component */])({
+        selector: 'app-card',
+        template: __webpack_require__("../../../../../src/app/card/card.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/card/card.component.css")],
+    }),
+    __metadata("design:paramtypes", [typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_2__card_service__["a" /* CardService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__card_service__["a" /* CardService */]) === "function" && _b || Object])
+], CardComponent);
+
+var _a, _b;
+//# sourceMappingURL=card.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/card/card.model.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Card; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var Card = (function () {
+    function Card(value, revealed, text) {
+        this.value = value;
+        this.revealed = revealed;
+        this.text = text;
+    }
+    return Card;
+}());
+Card = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["c" /* Injectable */])(),
+    __metadata("design:paramtypes", [Number, Boolean, String])
+], Card);
+
+//# sourceMappingURL=card.model.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/card/card.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return CardService; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var CardService = (function () {
+    function CardService() {
+    }
+    CardService.prototype.moveHorizontal = function (card) { };
+    CardService.prototype.moveVertical = function (card) { };
+    return CardService;
+}());
+CardService = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["c" /* Injectable */])(),
+    __metadata("design:paramtypes", [])
+], CardService);
+
+//# sourceMappingURL=card.service.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/game-functions.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__card_card_model__ = __webpack_require__("../../../../../src/app/card/card.model.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return GameFunctions; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+var GameFunctions = (function () {
+    function GameFunctions() {
+    }
+    GameFunctions.prototype.InitializeArray = function () {
+        this.gameArray = [];
+        for (var i = 0; i < 16; i++) {
+            var letter = '';
+            var no = void 0;
+            if (i < 4) {
+                no = i + 1;
+            }
+            else {
+                no = (i + 1) % 4;
+            }
+            if ((i + 1) % 4 === 0) {
+                no = 4;
+            }
+            if (i < 4) {
+                letter = 'S';
+            }
+            else if (i < 8) {
+                letter = 'C';
+            }
+            else if (i < 12) {
+                letter = 'H';
+            }
+            else {
+                letter = 'D';
+            }
+            var card = new __WEBPACK_IMPORTED_MODULE_0__card_card_model__["a" /* Card */](no, false, letter);
+            this.gameArray.push(card);
+        }
+        return this.gameArray;
+    };
+    GameFunctions.prototype.shuffleArray = function (a) {
+        for (var i = a.length; i; i--) {
+            var j = Math.floor(Math.random() * i);
+            _a = [a[j], a[i - 1]], a[i - 1] = _a[0], a[j] = _a[1];
+        }
+        var _a;
+    };
+    return GameFunctions;
+}());
+GameFunctions = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["c" /* Injectable */])()
+], GameFunctions);
+
+//# sourceMappingURL=game-functions.service.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
